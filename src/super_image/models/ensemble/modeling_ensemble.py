b83e6b3328d91c401aa808369e8aac60952dd215
emmuzoo <aztleclan@gmail.com>
2023-07-19 15:22:22 +0200

    ensamble: ensamble

--- src/super_image/models/ensemble/modeling_ensemble.py
+++ src/super_image/models/ensemble/modeling_ensemble.py
@@ -8,6 +8,7 @@ from ..mdsr.modeling_mdsr import MdsrModel
 from ..msrn.modeling_msrn import MsrnModel
 from .configuration_ensemble import EnsembleConfig
 from ...modeling_utils import (
+    default_conv,
     BamBlock,
     MeanShift,
     PreTrainedModel
@@ -17,9 +18,13 @@ from ...modeling_utils import (
 class EnsembleModel(PreTrainedModel):
     config_class = EnsembleConfig
 
-    def __init__(self, args):
+    def __init__(self, args, conv=default_conv):
         super(EnsembleModel, self).__init__(args)
         self.scale = args.scale
+        n_feats = 9
+        n_colors = 3
+        kernel_size = 3
+
         self.model_msrn = MsrnModel.from_pretrained('eugenesiow/msrn', scale=self.scale)
         for param in self.model_msrn.parameters():
           param.requires_grad = False
@@ -29,14 +34,16 @@ class EnsembleModel(PreTrainedModel):
         self.model_edrs_base = EdsrModel.from_pretrained('eugenesiow/edsr-base', scale=self.scale)
         for param in self.model_edrs_base.parameters():
           param.requires_grad = False
-        self.conv = nn.Conv2d(in_channels=9, out_channels=3, kernel_size=3, padding='same')
-
+        #self.conv = nn.Conv2d(in_channels=9, out_channels=3, kernel_size=3, padding='same')
+        self.tail = nn.Sequential([
+          conv(n_feats, n_colors, kernel_size)
+        ])
 
     def forward(self, x):
         x1 = self.model_msrn(x)
         x2 = self.model_mdrs(x)
         x3 = self.model_edrs_base(x)
         xall = torch.cat((x1, x2, x3), dim=1)
-        out = self.conv(xall)
+        out = self.tail(xall)
 
         return out
