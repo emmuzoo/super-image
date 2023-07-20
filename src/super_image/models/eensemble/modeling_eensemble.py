103c4414876f55aba1cbb5845ef707846af98be5
emmuzoo <aztleclan@gmail.com>
2023-07-21 01:01:56 +0200

    ensemble

--- src/super_image/models/eensemble/modeling_eensemble.py
+++ src/super_image/models/eensemble/modeling_eensemble.py
@@ -25,25 +25,42 @@ class EensembleModel(PreTrainedModel):
         n_colors = 3
         kernel_size = 3
 
-        self.model_msrn = MsrnModel.from_pretrained('eugenesiow/msrn', scale=self.scale)
-        for param in self.model_msrn.parameters():
+        backbone_msrn = MsrnModel.from_pretrained('eugenesiow/msrn', scale=self.scale)
+        for param in backbone_msrn.parameters():
           param.requires_grad = False
-        self.model_mdrs = MdsrModel.from_pretrained('eugenesiow/mdsr', scale=self.scale)
-        for param in self.model_mdrs.parameters():
-          param.requires_grad = False
-        self.model_edrs_base = EdsrModel.from_pretrained('eugenesiow/edsr-base', scale=self.scale)
-        for param in self.model_edrs_base.parameters():
-          param.requires_grad = False
-        #self.conv = nn.Conv2d(in_channels=9, out_channels=3, kernel_size=3, padding='same')
-        self.conv1 = conv(n_feats, n_colors, kernel_size)
-        self.conv2 = conv(n_feats, n_colors, kernel_size)
-        self.conv3 = conv(n_feats, n_colors, kernel_size)
         
+        backbone_mdrs = MdsrModel.from_pretrained('eugenesiow/mdsr', scale=self.scale)
+        for param in backbone_mdrs.parameters():
+          param.requires_grad = False
+        
+        backbone_edrs_base = EdsrModel.from_pretrained('eugenesiow/edsr-base', scale=self.scale)
+        for param in backbone_edrs_base.parameters():
+          param.requires_grad = False
+        
+        #self.conv = nn.Conv2d(in_channels=9, out_channels=3, kernel_size=3, padding='same')
+        #self.conv1 = conv(n_feats, n_colors, kernel_size)
+        #self.conv2 = conv(n_feats, n_colors, kernel_size)
+        #self.conv3 = conv(n_feats, n_colors, kernel_size)
+        
+        self.net1 = nn.Sequential(list(backbone_msrn.children()))
+        self.net1.add_module('conv11', conv(n_feats, n_colors, kernel_size))
+        self.net1.add_module('conv12', conv(n_feats, n_colors, kernel_size))
+
+        self.net2 = nn.Sequential(list(backbone_mdrs.children()))
+        self.net1.add_module('conv21', conv(n_feats, n_colors, kernel_size))
+        self.net1.add_module('conv22', conv(n_feats, n_colors, kernel_size))
+
+        self.net3 = nn.Sequential(list(backbone_edrs_base.children()))
+        self.net1.add_module('conv31', conv(n_feats, n_colors, kernel_size))
+        self.net1.add_module('conv32', conv(n_feats, n_colors, kernel_size))
 
     def forward(self, x):
-        x1 = self.conv1(self.model_msrn(x))
-        x2 = self.conv2(self.model_mdrs(x))
-        x3 = self.conv3(self.model_edrs_base(x))
+        #x1 = self.conv1(self.model_msrn(x))
+        #x2 = self.conv2(self.model_mdrs(x))
+        #x3 = self.conv3(self.model_edrs_base(x))
+        x1 = self.net1(x)
+        x2 = self.net2(x)
+        x3 = self.net3(x)
         out = torch.add(x1, x2)
         out = torch.add(out, x3)
 
